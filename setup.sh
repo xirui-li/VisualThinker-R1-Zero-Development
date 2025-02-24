7e00998cc490bab3af6ee673aed56ff82f1a0291
LewisZhou <41515593+HengguangZhou@users.noreply.github.com>
2025-02-23 20:55:02 -0800

    Add installing duckdb

--- setup.sh
+++ setup.sh
@@ -12,4 +12,5 @@ pip install qwen_vl_utils torchvision
 pip install flash-attn --no-build-isolation
 
 pip install git+https://github.com/huggingface/transformers.git # correct deepspeed support
+pip install duckdb
 
