8ef0cde80d2d03d71d43cd355886efc81988edbd
LewisZhou <41515593+HengguangZhou@users.noreply.github.com>
2025-02-23 20:47:14 -0800

    Update run_grpo_SAT.sh

--- src/open-r1-multimodal/run_grpo_SAT.sh
+++ src/open-r1-multimodal/run_grpo_SAT.sh
@@ -4,20 +4,20 @@ export CUDA_VISIBLE_DEVICES=0
 export MAIN_PROCESS_PORT=29507  # Change this to an available port
 
 accelerate launch --config_file=configs/zero2.yaml src/open_r1/grpo.py \
-    --output_dir outputs/Qwen2-VL-2B-GRPO-SAT-LLM-Freeze \
-    --model_name_or_path /nfs/data/xiruili/data_selection/pretrained_models/Qwen2-VL-2B-Instruct \
+    --output_dir outputs/Qwen2-VL-2B-GRPO-SAT \
+    --model_name_or_path Qwen2-VL-2B-Instruct \
     --dataset_name SAT \
-    --max_prompt_length 512 \
-    --max_completion_length 128 \
+    --max_prompt_length 1024 \
+    --max_completion_length 512 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 2 \
     --logging_steps 1 \
     --bf16 \
     --gradient_checkpointing false \
     --attn_implementation flash_attention_2 \
-    --max_pixels 156800 \
+    --max_pixels 401408 \
     --num_train_epochs 2 \
-    --run_name Qwen2-VL-2B-GRPO-SAT-LLM-Freeze \
+    --run_name Qwen2-VL-2B-GRPO-SAT \
     --save_steps 100 \
     --save_only_model true \
     --report_to wandb \
